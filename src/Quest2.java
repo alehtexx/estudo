f4e9472d0b5f30599a8c29344f5660e3377b0781
Alessandro Romerito <alessandroromerito2000@gmail.com>
2024-03-26 00:49:18 -0300

    adicionado questão dois

---
+++ src/Quest2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        public static void main(String[] args) throws IOException {
+            int A;
+            int B;
+            int X;
+            Scanner entrada = new Scanner(System.in);
+            A = entrada.nextInt();
+            B = entrada.nextInt();
+
+            X = A + B;
+
+            System.out.println("X = "+X);
+
+        }
+    }
