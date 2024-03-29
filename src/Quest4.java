b298eee078a4762b7d04f29d6b1fe33f8952e1e8
Alessandro Romerito <alessandroromerito2000@gmail.com>
2024-03-29 18:30:26 -0300

    comentario

---
+++ src/Quest4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int A;
+        int B;
+        int soma;
+        Scanner entrada = new Scanner(System.in);
+        A = entrada.nextInt();
+        B = entrada.nextInt();
+        soma = A + B;
+
+        System.out.printf("SOMA = "+soma+"\n");
+
+    }}
+
